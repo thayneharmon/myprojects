44fa418170b9ac7c71e8cb6cc9575259722c3da1
thayneharmon <tharmon@familysearch.org>
2015-11-10 08:38:39 -0700

    changes for apprentice badge
    added calc for interface and comp vs inherit

---
+++ Apprentice_apps/src/com/company/calculator/add.java
@@ -0,0 +1,14 @@
+package com.company.calculator;
+
+/**
+ * Created by thayneharmon on 11/9/2015.
+ */
+public class add extends Print {
+
+  public int doCalc(int a, int b) {
+    int c =  a + b;
+    System.out.println(a + " plus " + b);
+    sign(c);
+    return c;
+  }
+}
