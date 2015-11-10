28847794f7a56fdad7afaea926b41d72bffc570a
thayneharmon <tharmon@familysearch.org>
2015-11-10 12:22:09 -0700

    apprentice update
    interface

---
+++ Apprentice_apps/src/com/company/calculator/Print.java
@@ -0,0 +1,10 @@
+package com.company.calculator;
+
+/**
+ * Created by thayneharmon on 11/9/2015.
+ */
+public class Print {
+  public void sign(int t) {
+    System.out.println("Total = " + t);
+  }
+}
